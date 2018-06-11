a63304f9dc8e8230f2e95bc6f9f861608eaf3d4b
Alan Santiago <alanivan.s06@gmail.com>
2018-06-10 20:57:36 -0500

    Modificar la ruta para ser multilenguaje

--- src/extra/Constantes.java
+++ src/extra/Constantes.java
@@ -8,8 +8,8 @@ import notes.Paneles;
  * @author n1ght_m4re
  */
 public class Constantes {
-    public static final String IconPath = "/icon.png";
-    public static final String ReminderPath = System.getProperty("user.home") + "/Documentos/Reminder/";
+    public static String ReminderPath = System.getProperty("user.home") + "/Reminder/";
+    public static String IconPath = "/icon.png";
     public static Paneles paneles = new Paneles();
     public static Notas notas;
 }
